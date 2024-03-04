bf1fcccc048c150a3995b1a3cfdeaa706ed72982
Ha-Said <saidhajabdallahs@gmail.com>
2024-03-04 19:57:37 +0100

    finished addEntry

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ function cleanInputString(str) {
   return str.replace(regex, '');
 }
 
+
+
 function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
+
+
+
+function addEntry() {
+  // pick where I'm putting my content 
+  //(ex: breakfast you take the value of the dropdown then you put(breakfast) .input_container so you get 
+  // targetInputContainer=breakfast.input-container;
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+addEntryButton.addEventListener("click", addEntry); 
